7b597ac6820fc937d3cf2d314bf7be8e1de351a4
Damian Brzana <brzanad@gmail.com>
2023-04-25 15:56:31 +0200

    moved brzana from main.py to brzana.py

---
+++ brzana.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+# Alg wyznaczania zer niezależnych
+def brzana():
+    return 0    # macierz,
--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import numpy as np
 import random
+import brzana
 
 
 def main():
-    random.seed(1)
-    A = np.array([[random.randint(0, 9) for i in range(6)] for j in range(6)])
+    random.seed(1)      # Ziarno generatora
+    A = np.array([[random.randint(0, 9) for i in range(6)] for j in range(6)])   # Tworzymy losową macierz 6 x 6
 
     print("Macierz początkowa:\n{}".format(A))
+
+    brzana.brzana()
 
 
 # Redukcja macierzy
 def lewicka():
     return 0  # macierz, lower bound
-
-
-# Alg wyznaczania zer niezależnych
-def brzana():
-    return 0    # macierz,
 
 
 # Alg wykreślania zer macierzy minimalną liczbą linii
