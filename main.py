ff90f65dde2a3938ab60b57ea2611fd205fd5d36
Patrycj2a <107553775+Patrycj2a@users.noreply.github.com>
2023-05-06 20:07:54 +0200

    redukcja macierzy

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import numpy as np
 import random
+from typing import List
 
 
 def main():
     random.seed(1)
-    A = np.array([[random.randint(0, 9) for i in range(6)] for j in range(6)])
+    matrix = np.array([[random.randint(0, 9) for i in range(6)] for j in range(6)])
 
-    print("Macierz początkowa:\n{}".format(A))
+    print("Macierz początkowa:\n{}".format(matrix))
 
 
-# Redukcja macierzy
-def lewicka():
-    return 0  # macierz, lower bound
+#Redukcja macierzy
+def lewicka(matrix) ->  List[List]:
+
+    sum_reduction = 0
+    for row in range(len(matrix)):
+        min_value = min(matrix[row])
+        sum_reduction += min_value
+        for col in range(len(matrix)):
+            matrix[row][col]  -= min_value
+    
+    return matrix, sum_reduction #macierz, dolne ograniczenie
 
 
 # Alg wyznaczania zer niezależnych
